c72682b90efcd7fd2af3ec721785a39b8eba133c
chhwu <76684717+chhwu@users.noreply.github.com>
2021-01-11 18:47:05 +0800

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,12 +1,15 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
     ask = int(input('Guess a number: '))
+    count = count + 1
     if ask < r:
         print('Guess bigger.')
     elif ask > r:
         print('Guess smaller.')
     else:
         print('Great guess!')
+        print('Guess:', count, 'times.')
         break
